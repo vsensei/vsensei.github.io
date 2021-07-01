7c9d7388c49f5c75d0dd8e3bcec725199fb370fb
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-01 21:58:41 +0300

    added maininfo component

---
+++ src/components/main-info/main-info.component.js
@@ -0,0 +1,32 @@
+import './main-info.styles.scss';
+
+const MainInfo = () => {
+  return (
+    <div className="maininfo">
+      <div className="about">
+        <div className="avatar">
+          <img src="images/avatar.jpg" alt="avatar" />
+        </div>
+        <div className="my-info">
+          <span>Nikita Afanasiev</span>
+          <span>
+            <i>@vinishkosensei</i>
+          </span>
+          <span>Sample Text</span>
+        </div>
+      </div>
+      <div className="tech-info">
+        <div>react</div>
+        <div>redux</div>
+        <div>...</div>
+        <div>...</div>
+        <div>...</div>
+        <div>...</div>
+        <div>...</div>
+        <div>...</div>
+      </div>
+    </div>
+  );
+};
+
+export default MainInfo;
