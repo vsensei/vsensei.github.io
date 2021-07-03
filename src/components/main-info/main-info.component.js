19f95bee6b6bc7dd1319696221a640c7e0af8a12
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-03 05:40:58 +0300

    changed main-info component

--- src/components/main-info/main-info.component.js
+++ src/components/main-info/main-info.component.js
@@ -1,11 +1,35 @@
 import './main-info.styles.scss';
+import technologies from '../../information/technologies';
+import TechInfo from '../tech-info/tech-info.component';
 
-const MainInfo = () => {
+const MainInfo = ({
+  allFilters,
+  setAllFilters,
+  filteredTechnologies,
+  setFilteredTechnologies,
+  githubProfile,
+}) => {
+  const filterTechnologies = (tech) => () => {
+    setFilteredTechnologies({
+      ...filteredTechnologies,
+      [tech]: !filteredTechnologies[tech],
+    });
+    setAllFilters(false);
+  };
+  const clearTechnologiesFilter = () => {
+    setFilteredTechnologies(
+      Object.fromEntries(technologies.map((tech) => [[tech], false]))
+    );
+    setAllFilters((oldAllFilters) => !oldAllFilters);
+  };
+
   return (
     <div className="maininfo">
       <div className="about">
         <div className="avatar">
-          <img src="images/avatar.jpg" alt="avatar" />
+          <a href={githubProfile} target="_blank" rel="noreferrer nofollow">
+            <img src="images/avatar.jpg" alt="avatar" />
+          </a>
         </div>
         <div className="my-info">
           <span>VSensei</span>
@@ -15,16 +39,12 @@ const MainInfo = () => {
           <span>Sample Text</span>
         </div>
       </div>
-      <div className="tech-info-container">
-        <div className="tech-info">react</div>
-        <div className="tech-info">redux</div>
-        <div className="tech-info">...</div>
-        <div className="tech-info">...</div>
-        <div className="tech-info">...</div>
-        <div className="tech-info">...</div>
-        <div className="tech-info">...</div>
-        <div className="tech-info">...</div>
-      </div>
+      <TechInfo
+        filteredTechnologies={filteredTechnologies}
+        allFilters={allFilters}
+        clearTechnologiesFilter={clearTechnologiesFilter}
+        filterTechnologies={filterTechnologies}
+      />
     </div>
   );
 };
