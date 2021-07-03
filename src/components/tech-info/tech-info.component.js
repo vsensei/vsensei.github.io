58984e4c986ef7d158c46157c0f82845febda192
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-03 05:39:27 +0300

    added tech-info component

---
+++ src/components/tech-info/tech-info.component.js
@@ -0,0 +1,47 @@
+import './tech-info.styles.scss';
+
+const TechInfo = ({
+  technologies,
+  filteredTechnologies,
+  allFilters,
+  clearTechnologiesFilter,
+  filterTechnologies,
+}) => {
+  return (
+    <div className="tech-info-container">
+      {technologies ? (
+        <div className="tech-info-container">
+          {technologies.map((tech, index) => (
+            <div className="tech-info" key={index}>
+              {tech}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <>
+          <div
+            className={`tech-info${allFilters ? ' chosen' : ''}`}
+            onClick={clearTechnologiesFilter}
+          >
+            All
+          </div>
+          {Object.keys(filteredTechnologies).map((tech, index) => {
+            return (
+              <div
+                className={`tech-info${
+                  filteredTechnologies[tech] ? ' chosen' : ''
+                }`}
+                key={index}
+                onClick={filterTechnologies(tech)}
+              >
+                {tech}
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TechInfo;
