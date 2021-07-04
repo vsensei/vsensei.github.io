ed467647ce7429246cbf2fa6c3c19ae46499f8d4
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-04 21:45:11 +0300

    changed info

--- src/information/profileinfo.js
+++ src/information/profileinfo.js
@@ -1,3 +1,4 @@
-const profileInfo = 'Sample Text';
+const profileInfo =
+  'In projects I use React, Redux, Redux-sagas, NodeJS, Express/Hapi. You can check out my projects and their source code below.';
 
 export default profileInfo;
